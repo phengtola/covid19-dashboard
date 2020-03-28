2f0e3ce7caf06972b9d2b54d0670650427dadb75
Pheng Tola <tolapheng99@gmail.com>
2020-03-28 13:39:23 +0700

    update

--- web-data-connector-tableau/Examples/js/covidWorldData.js
+++ web-data-connector-tableau/Examples/js/covidWorldData.js
@@ -36,7 +36,7 @@
             id: "serious_critical",
             alias: "serious_critical",
             dataType: tableau.dataTypeEnum.float
-        }, {
+        }/*, {
             id: "tot_cases1m_pop",
             alias: "tot_cases1m_pop",
             dataType: tableau.dataTypeEnum.float
@@ -44,7 +44,7 @@
             id: "tot_deaths1m_pop",
             alias: "tot_deaths1m_pop",
             dataType: tableau.dataTypeEnum.float
-        }/*, {
+        }, {
             id: "updated_at",
             alias: "updated_at",
             dataType: tableau.dataTypeEnum.date
@@ -74,9 +74,9 @@
                     "new_deaths": feat.new_deaths,
                     "total_recovered": feat.total_recovered,
                     "active_cases": feat.active_cases,
-                    "serious_critical": feat.serious_critical,
-                    "tot_cases1m_pop": feat.tot_cases1m_pop,
-                    "tot_deaths1m_pop": feat.tot_deaths1m_pop
+                    "serious_critical": feat.serious_critical
+                    //"tot_cases1m_pop": feat.tot_cases1m_pop,
+                    //"tot_deaths1m_pop": feat.tot_deaths1m_pop,
                    // "updated_at": feat.updated_at
                 });
             console.log("tableData", tableData);
